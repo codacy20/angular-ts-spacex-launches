c2b94f0171783be28203df1169d14dc012942407
codacy20 <kmuc72@gmail.com>
2020-08-16 17:16:45 +0200

    service layer v1

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Got } from "got";
 
 @Injectable()
 export class UserServiceService {
+  constructor(private got: Got) {}
 
-  constructor() { }
-
+  getUsersList() {
+    (async () => {
+      try {
+        const response = await this.got.get("https://sindresorhus.com");
+        console.log(response);
+        //=> '<!doctype html> ...'
+      } catch (error) {
+        console.log(error.response.body);
+        //=> 'Internal server error ...'
+      }
+    })();
+  }
 }
