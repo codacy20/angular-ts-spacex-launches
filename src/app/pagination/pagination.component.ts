5db310f5484839c0f6bc9e4012eaba6acf3838c1
codacy20 <kmuc72@gmail.com>
2020-08-19 13:44:36 +0200

    3 files modified

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.css']
+  selector: "app-pagination",
+  templateUrl: "./pagination.component.html",
+  styleUrls: ["./pagination.component.css"]
 })
 export class PaginationComponent implements OnInit {
+  active = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  move(input: number) {
+    if (input === 0 && this.active > 0) this.active--;
+    if (input === 1 && this.active < 2) this.active++;
   }
-
 }
